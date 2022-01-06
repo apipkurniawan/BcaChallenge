cf45cde82fa451481cb67c7f57ad20a01892a82b
apipkurniawan <davitaja92@gmail.com>
2022-01-06 10:23:49 +0700

    configurasi tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
