c00748b1be75df49b0a69eaadeea9626abc00dd4
apipkurniawan <davitaja92@gmail.com>
2022-01-08 18:07:01 +0700

    create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { Challenge1Component } from "./features/challenge_1/challenge1.component";
+import { Challenge2Component } from "./features/challenge_2/challenge2.component";
 import { DashboardComponent } from "./features/dashborad/dashborad.component";
 
 const routes: Routes = [
   { path: "", component: DashboardComponent },
-  { path: "bca", component: Challenge1Component },
+  { path: "bca-css", component: Challenge1Component },
+  { path: "bca-tailwind", component: Challenge2Component },
 ];
 
 @NgModule({
